961e1bb0e48d8441c24d68a7d292e43043fd90a1
Ahmed Drioueche <adsrahmed@gmail.com>
2024-10-01 01:53:22 +0100

    update

--- src/app/services/geminiService.ts
+++ src/app/services/geminiService.ts
@@ -1,11 +1,11 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
 const API_KEY: string | undefined = process.env.GEMINI_KEY;
-console.log('api_key', API_KEY);
 const genAI = API_KEY? new GoogleGenerativeAI(API_KEY) : null;
 const model = genAI?.getGenerativeModel({ model: 'gemini-1.5-flash' });
 
 export const getGeminiAnswer = async (prompt: string) => {
+    console.log('api_key', API_KEY);
     console.log('prompt', prompt);
     const result = await model?.generateContent(prompt);
     
