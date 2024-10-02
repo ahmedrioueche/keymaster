48e79038d69d4928ce90f033e4286af82deb8ac1
Ahmed Drioueche <adsrahmed@gmail.com>
2024-10-02 03:14:41 +0100

    update

--- src/app/services/userService.ts
+++ src/app/services/userService.ts
@@ -1,29 +1,29 @@
-import { User } from "../types/types";
-//import prisma from "../utils/prisma";
+  import { User } from "../types/types";
+  import prisma from "../utils/prisma";
 
-export const insertUser = async (user: User) => {
-  console.log("user", user);
- // try {
- //   // Insert user into the database
- //   const newUser = await prisma.user.create({
- //     data: {
- //       name: user.name,
- //       rank: user.rank ?? null, // Optional field
- //       speed: user.speed ?? null, // Optional field
- //       lastEntryDate: user.lastEntryDate ? new Date(user.lastEntryDate) : null, // Optional field and converted to Date type
- //       typingStats: {
- //         create: user.typingStats?.map(stat => ({
- //           accuracy: stat.accuracy,
- //           speed: stat.speed
- //         })) || [] // Handle optional typingStats field
- //       }
- //     }
- //   });
-//
- //   console.log('User inserted successfully:', newUser);
- //   return newUser;
- // } catch (error) {
- //   console.error('Error inserting user:', error);
- //   throw error;
- // }
-};
+  export const insertUser = async (user: User) => {
+    console.log("user", user);
+   try {
+     // Insert user into the database
+     const newUser = await prisma.user.create({
+       data: {
+         name: user.name,
+         rank: user.rank ?? null, // Optional field
+         speed: user.speed ?? null, // Optional field
+         lastEntryDate: user.lastEntryDate ? new Date(user.lastEntryDate) : null, // Optional field and converted to Date type
+         typingStats: {
+           create: user.typingStats?.map(stat => ({
+             accuracy: stat.accuracy,
+             speed: stat.speed
+           })) || [] // Handle optional typingStats field
+         }
+       }
+     });
+  
+     console.log('User inserted successfully:', newUser);
+     return newUser;
+   } catch (error) {
+     console.error('Error inserting user:', error);
+     throw error;
+   }
+  };
