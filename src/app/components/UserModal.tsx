4ab1ef99461f3e459c13ec671a83642c85438f45
Ahmed Drioueche <adsrahmed@gmail.com>
2024-10-02 19:23:09 +0100

    update

--- src/app/components/UserModal.tsx
+++ src/app/components/UserModal.tsx
@@ -14,11 +14,13 @@ const UserModal: React.FC<UserModalProps> = ({ isOpen, onClose, onUserChange }) 
   const [currentUser, setCurrentUser] = useState<string | null>(null);
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>(''); // State for confirm password
   const [isChangingUser, setIsChangingUser] = useState(false);
+  const [isSignup, setIsSignup] = useState(false); // State for toggling between login and signup
 
   // Placeholder users array
   const previousUsers: User[] = [
-  
+    // Add your previous users here if any
   ];
 
   useEffect(() => {
@@ -43,10 +45,11 @@ const UserModal: React.FC<UserModalProps> = ({ isOpen, onClose, onUserChange }) 
     }
 
     localStorage.setItem("currentUser", JSON.stringify(newUser));
-    onUserChange? onUserChange(newUser) : null;
+    onUserChange ? onUserChange(newUser) : null;
     setCurrentUser(username);
     setUsername('');
     setPassword('');
+    setConfirmPassword(''); // Reset confirm password
   };
 
   const handleLogout = () => {
@@ -57,10 +60,8 @@ const UserModal: React.FC<UserModalProps> = ({ isOpen, onClose, onUserChange }) 
 
   const handleChangeUser = () => {
     if (previousUsers.length === 0) {
-      // If users array is empty, go directly to the login form
       setIsChangingUser(false);
     } else {
-      // Otherwise, show user selection screen
       handleLogout();
       setIsChangingUser(true);
     }
@@ -70,6 +71,14 @@ const UserModal: React.FC<UserModalProps> = ({ isOpen, onClose, onUserChange }) 
     setIsChangingUser(false);
   };
 
+  // Toggle between login and signup forms
+  const toggleSignup = () => {
+    setIsSignup(!isSignup);
+    setUsername(''); // Clear the username field
+    setPassword(''); // Clear the password field
+    setConfirmPassword(''); // Clear the confirm password field
+  };
+
   return (
     <div
       className={`fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 transition-transform duration-300 ${isOpen ? 'scale-100 opacity-100' : 'scale-0 opacity-0'}`}
@@ -77,7 +86,7 @@ const UserModal: React.FC<UserModalProps> = ({ isOpen, onClose, onUserChange }) 
       <div className="bg-light-background dark:bg-dark-background rounded-lg shadow-lg p-5 w-full sm:w-[90%] max-w-md max-h-[95vh] overflow-y-auto hide-scrollbar">
         <div className="flex justify-between items-center mb-4">
           <div className="flex items-center text-light-foreground dark:text-dark-foreground">
-           <Image src='/icons/profile.png' height={30} width={30} className="text-3xl mr-3" alt="Profile" />
+            <Image src='/icons/profile.png' height={30} width={30} className="text-3xl mr-3" alt="Profile" />
             <h2 className="text-xl font-bold mt-1 font-stix">User</h2>
           </div>
           <button
@@ -90,46 +99,43 @@ const UserModal: React.FC<UserModalProps> = ({ isOpen, onClose, onUserChange }) 
 
         {/* Conditional rendering for user change screen */}
         {isChangingUser ? (
-          <>
-            <div className="text-center">
-              <Image src="/storysets/add-user.svg" alt="Add user illustration" className="w-full h-48 object-contain" height={48} width={38}/>
-              <h3 className="text-xl font-bold mb-4 text-dark-background dark:text-light-background ">Select a User</h3>
-              <div className="space-y-2">
-                {previousUsers.map((user, index) => (
-                  <button
-                    key={index}
-                    className="w-full px-4 py-3 bg-light-secondary text-dark-background rounded-md font-semibold hover:text-light-background hover:bg-light-accent dark:hover:bg-dark-accent transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-light-secondary dark:ring-dark-secondary focus:ring-offset-2"
-                  >
-                    {user.name}
-                  </button>
-                ))}
-              </div>
-              <div className='flex flex-row justify-between mt-5'>
+          <div className="text-center">
+            <Image src="/storysets/add-user.svg" alt="Add user illustration" className="w-full h-48 object-contain" height={48} width={38}/>
+            <h3 className="text-xl font-bold mb-4 text-dark-background dark:text-light-background ">Select a User</h3>
+            <div className="space-y-2">
+              {previousUsers.map((user, index) => (
                 <button
-                  type="button"
-                  className="flex-1 px-4 py-3 bg-light-secondary text-dark-background rounded-md font-semibold hover:text-light-background hover:bg-light-accent dark:hover:bg-dark-accent transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-light-secondary dark:ring-dark-secondary focus:ring-offset-2 mr-2"
-                  onClick={handleBackToLogin}
+                  key={index}
+                  className="w-full px-4 py-3 bg-light-secondary text-dark-background rounded-md font-semibold hover:text-light-background hover:bg-light-accent dark:hover:bg-dark-accent transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-light-secondary dark:ring-dark-secondary focus:ring-offset-2"
                 >
-                  Back
+                  {user.name}
                 </button>
-                <button
-                  type="button"
-                  className="flex-1 px-4 py-3 bg-light-secondary text-dark-background rounded-md font-semibold hover:text-light-background hover:bg-light-accent dark:hover:bg-dark-accent transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-light-secondary dark:ring-dark-secondary focus:ring-offset-2 ml-2"
-                  onClick={handleLogout}
-                >
-                  New User
-                </button>
-              </div>
+              ))}
             </div>
-          </>
+            <div className='flex flex-row justify-between mt-5'>
+              <button
+                type="button"
+                className="flex-1 px-4 py-3 bg-light-secondary text-dark-background rounded-md font-semibold hover:text-light-background hover:bg-light-accent dark:hover:bg-dark-accent transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-light-secondary dark:ring-dark-secondary focus:ring-offset-2 mr-2"
+                onClick={handleBackToLogin}
+              >
+                Back
+              </button>
+              <button
+                type="button"
+                className="flex-1 px-4 py-3 bg-light-secondary text-dark-background rounded-md font-semibold hover:text-light-background hover:bg-light-accent dark:hover:bg-dark-accent transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-light-secondary dark:ring-dark-secondary focus:ring-offset-2 ml-2"
+                onClick={handleLogout}
+              >
+                New User
+              </button>
+            </div>
+          </div>
         ) : (
-          <>
-            {!currentUser && (
+          <div>
+            {isSignup ? ( // Render signup form if isSignup is true
               <>
                 <div className="mt-0">
-                  <Image src="/storysets/user.svg" alt="Login illustration" className="w-full h-48 object-contain" height={48} width={38}/>
+                  <Image src="/storysets/user.svg" alt="Signup illustration" className="w-full h-48 object-contain" height={48} width={38}/>
                 </div>
-
                 <form onSubmit={handleUserSubmit} className="space-y-4">
                   <div>
                     <input
@@ -153,43 +159,85 @@ const UserModal: React.FC<UserModalProps> = ({ isOpen, onClose, onUserChange }) 
                       required
                     />
                   </div>
+                  <div>
+                    <input
+                      id="confirmPassword"
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                      type="password"
+                      className="w-full p-3 border border-gray-300 rounded-md dark:bg-gray-700 dark:border-gray-600 dark:text-white font-stix focus:ring-2 focus:ring-light-secondary focus:outline-none focus:border-transparent"
+                      placeholder="Confirm your password"
+                      required
+                    />
+                  </div>
                   <button
                     type="submit"
                     className="w-full px-4 py-3 mt-5 bg-light-secondary text-white rounded-md font-semibold hover:text-dark-background hover:bg-light-accent dark:hover:bg-dark-accent transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-light-secondary dark:ring-dark-secondary focus:ring-offset-2"
                   >
-                    Log In
+                    Sign Up
                   </button>
                 </form>
+                <div className="mt-4 text-center">
+                  <p className="text-gray-600 dark:text-gray-300">Already have an account? <span className="text-light-secondary cursor-pointer" onClick={toggleSignup}>Login</span></p>
+                </div>
+              </>
+            ) : (
+              <>
+                {!currentUser && (
+                  <>
+                    <div className="mt-0">
+                      <Image src="/storysets/user.svg" alt="Login illustration" className="w-full h-48 object-contain" height={48} width={38}/>
+                    </div>
+
+                    <form onSubmit={handleUserSubmit} className="space-y-4">
+                      <div>
+                        <input
+                          id="username"
+                          value={username}
+                          onChange={(e) => setUsername(e.target.value)}
+                          type="text"
+                          className="w-full p-3 border border-gray-300 rounded-md dark:bg-gray-700 dark:border-gray-600 dark:text-white font-stix focus:ring-2 focus:ring-light-secondary focus:outline-none focus:border-transparent"
+                          placeholder="Enter your username"
+                          required
+                        />
+                      </div>
+                      <div>
+                        <input
+                          id="password"
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                          type="password"
+                          className="w-full p-3 border border-gray-300 rounded-md dark:bg-gray-700 dark:border-gray-600 dark:text-white font-stix focus:ring-2 focus:ring-light-secondary focus:outline-none focus:border-transparent"
+                          placeholder="Enter your password"
+                          required
+                        />
+                      </div>
+                      <button
+                        type="submit"
+                        className="w-full px-4 py-3 mt-5 bg-light-secondary text-white rounded-md font-semibold hover:text-dark-background hover:bg-light-accent dark:hover:bg-dark-accent transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-light-secondary dark:ring-dark-secondary focus:ring-offset-2"
+                      >
+                        Login
+                      </button>
+                    </form>
+                    <div className="mt-4 text-center">
+                      <p className="text-gray-600 dark:text-gray-300">Don't have an account? <span className="text-light-secondary cursor-pointer" onClick={toggleSignup}>Sign Up</span></p>
+                    </div>
+                  </>
+                )}
+                {currentUser && (
+                  <div className="text-center">
+                    <h3 className="text-xl font-bold mb-4 text-dark-background dark:text-light-background">Welcome, {currentUser}!</h3>
+                    <button
+                      onClick={handleLogout}
+                      className="w-full px-4 py-3 bg-light-secondary text-white rounded-md font-semibold hover:text-dark-background hover:bg-light-accent dark:hover:bg-dark-accent transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-light-secondary dark:ring-dark-secondary focus:ring-offset-2"
+                    >
+                      Logout
+                    </button>
+                  </div>
+                )}
               </>
             )}
-
-            {currentUser && (
-              <div className="mt-6 text-center">
-                <div className="mb-2">
-                  <Image src="/storysets/logged-in.svg" alt="Logged-in" className="w-full h-48 object-contain" height={48} width={38}/>
-                </div>            
-                <p className="text-xl text-gray-600 dark:text-gray-300 font-stix mt-1">
-                  Logged in as: <strong>{currentUser}</strong>
-                </p>
-                <div className='flex flex-row justify-between mt-5'>
-                  <button
-                    type="button"
-                    className="flex-1 px-4 py-3 bg-light-secondary text-dark-background rounded-md font-semibold hover:text-light-background hover:bg-light-accent dark:hover:bg-dark-accent transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-light-secondary dark:ring-dark-secondary focus:ring-offset-2 mr-2"
-                    onClick={handleChangeUser}
-                  >
-                    Change User
-                  </button>
-                  <button
-                    type="button"
-                    className="flex-1 px-4 py-3 bg-light-secondary text-dark-background rounded-md font-semibold hover:text-light-background hover:bg-light-accent dark:hover:bg-dark-accent transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-light-secondary dark:ring-dark-secondary focus:ring-offset-2 ml-2"
-                    onClick={handleLogout}
-                  >
-                    Log Out
-                  </button>
-                </div>
-              </div>
-            )}
-          </>
+          </div>
         )}
       </div>
     </div>
