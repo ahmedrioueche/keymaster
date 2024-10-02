4a21baa36fce6e508d3cdaa61ee3079b3a02ee0d
Ahmed Drioueche <adsrahmed@gmail.com>
2024-10-02 03:09:15 +0100

    update

--- src/app/services/userService.ts
+++ src/app/services/userService.ts
@@ -1,28 +1,28 @@
 import { User } from "../types/types";
-import prisma from "../utils/prisma";
+//import prisma from "../utils/prisma";
 
 export const insertUser = async (user: User) => {
-  try {
-    // Insert user into the database
-    const newUser = await prisma.user.create({
-      data: {
-        name: user.name,
-        rank: user.rank ?? null, // Optional field
-        speed: user.speed ?? null, // Optional field
-        lastEntryDate: user.lastEntryDate ? new Date(user.lastEntryDate) : null, // Optional field and converted to Date type
-        typingStats: {
-          create: user.typingStats?.map(stat => ({
-            accuracy: stat.accuracy,
-            speed: stat.speed
-          })) || [] // Handle optional typingStats field
-        }
-      }
-    });
-
-    console.log('User inserted successfully:', newUser);
-    return newUser;
-  } catch (error) {
-    console.error('Error inserting user:', error);
-    throw error;
-  }
+ // try {
+ //   // Insert user into the database
+ //   const newUser = await prisma.user.create({
+ //     data: {
+ //       name: user.name,
+ //       rank: user.rank ?? null, // Optional field
+ //       speed: user.speed ?? null, // Optional field
+ //       lastEntryDate: user.lastEntryDate ? new Date(user.lastEntryDate) : null, // Optional field and converted to Date type
+ //       typingStats: {
+ //         create: user.typingStats?.map(stat => ({
+ //           accuracy: stat.accuracy,
+ //           speed: stat.speed
+ //         })) || [] // Handle optional typingStats field
+ //       }
+ //     }
+ //   });
+//
+ //   console.log('User inserted successfully:', newUser);
+ //   return newUser;
+ // } catch (error) {
+ //   console.error('Error inserting user:', error);
+ //   throw error;
+ // }
 };
