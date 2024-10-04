0dd64abf816d8fd052750ce213bffe3058438745
Ahmed Drioueche <adsrahmed@gmail.com>
2024-10-04 13:29:31 +0100

    update

--- src/app/components/TypingArea.tsx
+++ src/app/components/TypingArea.tsx
@@ -157,7 +157,7 @@ const TypingArea: React.FC<TypingAreaProps> = ({ textToType, isStarted, onComple
           lineHeight: '1.5',
           color: 'transparent',
           zIndex: 1,
-          height: `${textLength? Math.max(60, textLength * 3) : 200}px`, // Set height proportional to userTextLength
+          height: `${isStarted? textLength? Math.max(60, textLength * 3) : 200 : 200}px`, // Set height proportional to userTextLength
         }}
       >
         {userInput}
